b57f7dbcb5cc8cfde440ede17eb6f4b0bf5487d9
danielmtro <102640424+danielmtro@users.noreply.github.com>
2023-01-23 15:27:53 +1100

    detected most balls

--- ballrecognition.py
+++ ballrecognition.py
@@ -71,7 +71,7 @@ def get_pool_table_contours(image):
     lower_green = np.array([30,140,0])
     upper_green = np.array([100,255,255])
 
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(img_hsv, lower_green, upper_green)
 
     # Generate contours based on our mask
@@ -81,14 +81,40 @@ def get_pool_table_contours(image):
 
 def main():
 
-    ksize = (10, 10)
-    blur = cv2.blur(img, ksize)
+    img = cv2.imread("testImage.jpg")
+    img = cv2.resize(img, (1280, 800))
 
     contours, hierachy = get_pool_table_contours(img)
-    new_img = isolate_bounding_box(contours, img)
+    img = isolate_bounding_box(contours, img)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	
+    # Threshold of green in HSV space
+    lower_green = np.array([40, 40, 40])
+    upper_green = np.array([70, 255, 255])
+
+    # preparing the mask to overlay
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+
+    # The black region in the mask has the value of 0,
+    # so when multiplied with original image removes all non-blue regions
+    result = cv2.bitwise_and(img, img, mask = mask)
+    
+    result = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+    # Detect circles in the image using the HoughCircles function
+    circles = cv2.HoughCircles(result, cv2.HOUGH_GRADIENT, 1, 50, param1=90, param2=13, minRadius=3, maxRadius=30)
+
+    # Draw the circles on the image
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(img, (x, y), r, (0, 255, 0), 2)
+
 
     while True:
-        cv2.imshow("iamge", new_img)
+        cv2.imshow("iamge", img)
         q = cv2.waitKey(1)
         if q==ord("q"):
             break
